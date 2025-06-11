3b33fedb8a98df83f63cc7e748317d7885ebf3cf
jws02 <jws020319@naver.com>
2025-06-11 18:45:32 +0900

    alarm

--- app/src/main/java/com/example/myapp/model/AppDatabase.kt
+++ app/src/main/java/com/example/myapp/model/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.myapp.model
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
@@ -20,4 +22,21 @@ import androidx.room.TypeConverters
 abstract class AppDatabase : RoomDatabase() {
     abstract fun routineDao(): RoutineDao
     abstract fun routineRecordDao(): RoutineRecordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app-db"
+                ).fallbackToDestructiveMigration(true).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
